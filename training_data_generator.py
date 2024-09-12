51ebdfd10f9d63fb795dc13d0e21a0d71e71c86e
RodrigoKasama <rodrigo.parracho@gmail.com>
2024-09-11 21:50:56 -0300

    Limpeza geral do código e Compactação do gerador de dados de treinamento

--- training_data_generator.py
+++ training_data_generator.py
@@ -1,102 +1,108 @@
 import argparse
 import os
-from builders.data_training_builders import (sort_by_number,get_file_path,create_dataframe,create_directory_if_does_not_exist)
+from builders.data_training_builders import (
+    sort_by_number, get_file_path, create_dataframe, create_directory_if_does_not_exist)
 from builders.data_training_generators import generate_activities
 import numpy as np
 
+data_arrays_time_domain = [[] for _ in range(8)]
+data_arrays_frequency_domain = [[] for _ in range(8)]
+labels_list = [[] for _ in range(4)]
 
-''' EXECUTION OF THE TRAINING DATA GENERATION PROGRAM '''
+""" EXECUTION OF THE TRAINING DATA GENERATION PROGRAM """
 
 parser = argparse.ArgumentParser(description="Script for generating training data")
-parser.add_argument('position', type=str, choices=['chest', 'left', 'right'], help='Sensor position')
+parser.add_argument("position", type=str, choices=["chest", "left", "right"], help="Sensor position")
 args = parser.parse_args()
-
 position = args.position.upper()
 
 current_directory = os.path.dirname(__file__)
-
-main_directory = os.path.join(current_directory, 'database')
+main_directory = os.path.join(current_directory, "database")
 
 subdirectory_list = os.listdir(main_directory)
 
 subdirectory_list.sort(key=sort_by_number)
 
-if position == "CHEST":
+label_directory = os.path.join(current_directory, "labels_and_data", "labels", position.lower())
+data_array_directory = os.path.join(current_directory, "labels_and_data", "data", position.lower())
 
-    label_directory = os.path.join(current_directory, 'labels_and_data','labels','chest')
-    data_array_directory = os.path.join(current_directory, 'labels_and_data','data','chest')
+print("Criando diretórios de labels e data_arrays...")
+create_directory_if_does_not_exist(label_directory)
+create_directory_if_does_not_exist(data_array_directory)
 
-    create_directory_if_does_not_exist(label_directory)
-    create_directory_if_does_not_exist(data_array_directory)
-
-elif position == "LEFT":
-
-    label_directory = os.path.join(current_directory, 'labels_and_data', 'labels', 'left')
-    data_array_directory = os.path.join(current_directory, 'labels_and_data', 'data', 'left')
-
-    create_directory_if_does_not_exist(label_directory)
-    create_directory_if_does_not_exist(data_array_directory)
-else:
-    label_directory = os.path.join(current_directory, 'labels_and_data', 'labels', 'right')
-    data_array_directory = os.path.join(current_directory, 'labels_and_data', 'data', 'right')
-
-    create_directory_if_does_not_exist(label_directory)
-    create_directory_if_does_not_exist(data_array_directory)
-
-
-list_of_data_arrays_in_the_time_domain = [[] for _ in range(8)]
-
-list_of_data_arrays_in_the_frequency_domain = [[] for _ in range(8)]
-
-labels_list = [[] for _ in range(4)]
-
-print("comparação realizada")
+print("Obtendo os dados de cada usuário...")
 for subdirectory in subdirectory_list:
-    acc,gyr,sampling = get_file_path(main_directory, subdirectory, position)
-    acc_dataframe, gyr_dataframe, sampling_dataframe = create_dataframe(acc, gyr, sampling)
+    print(f"  {subdirectory}...", end="")
+    acc, gyr, sampling = get_file_path(main_directory, subdirectory, position)
+
+    acc_dataframe, gyr_dataframe, sampling_dataframe = create_dataframe(
+        acc, gyr, sampling)
+
     generate_activities(acc_dataframe, gyr_dataframe, sampling_dataframe, position,
-                        list_of_data_arrays_in_the_time_domain, list_of_data_arrays_in_the_frequency_domain,
-                        labels_list)
+                        data_arrays_time_domain, data_arrays_frequency_domain, labels_list)
+    print(f"OK")
 
-np.save(os.path.join(label_directory, 'multiple_class_label_1.npy'), np.asarray(labels_list[0]))
-np.save(os.path.join(label_directory, 'multiple_class_label_2.npy'), np.asarray(labels_list[1]))
-np.save(os.path.join(label_directory, 'binary_class_label_1.npy'), np.asarray(labels_list[2]))
-np.save(os.path.join(label_directory, 'binary_class_label_2.npy'), np.asarray(labels_list[3]))
+print("Salvando rótulos de cada caso...")
+for i in range(4):
+    prefix = "multiple" if i < 2 else "binary"
+    sufix = "1" if (i % 2) + 1 == 1 else "2"
+    np.save(os.path.join(label_directory,
+            f"{prefix}_class_label_{sufix}.npy"), np.asarray(labels_list[i]))
 
-np.save(os.path.join(data_array_directory, 'magacc_time_domain_data_array.npy'), np.asarray(list_of_data_arrays_in_the_time_domain[0]))
-np.save(os.path.join(data_array_directory, 'magacc_frequency_domain_data_array.npy'), np.asarray(list_of_data_arrays_in_the_frequency_domain[0]))
+"""
+# Porque desses pares?
+ 	# magacc - 0
+		# time_domain
+		# frequency_domain
+  
+	# acc_x_y_z_axes - 123
+		# time_domain
+		# frequency_domain
+  
+	# maggyr - 4
+		# time_domain
+		# frequency_domain
+  
+	# gyr_x_y_z_axes - 567
+		# time_domain
+		# frequency_domain
+  
+	# magacc_and_maggyr - 04
+		# time_domain
+		# frequency_domain
+  
+	# acc_and_gyr_three_axes - 123567
+		# time_domain
+		# frequency_domain
+"""
 
-np.save(os.path.join(data_array_directory, 'acc_x_y_z_axes_time_domain_data_array.npy'),
-        np.concatenate((np.asarray(list_of_data_arrays_in_the_time_domain[1]), np.asarray(list_of_data_arrays_in_the_time_domain[2]),
-                        np.asarray(list_of_data_arrays_in_the_time_domain[3])), axis=2))
 
-np.save(os.path.join(data_array_directory, 'acc_x_y_z_axes_frequency_domain_data_array.npy'),
-        np.concatenate((np.asarray(list_of_data_arrays_in_the_frequency_domain[1]), np.asarray(list_of_data_arrays_in_the_frequency_domain[2]),
-                        np.asarray(list_of_data_arrays_in_the_frequency_domain[3])), axis=2))
+print("Criando os arquivos .npy referente a cada agrupamento de dados...")
+for topic, font in {"time": data_arrays_time_domain, "frequency": data_arrays_frequency_domain}.items():
 
-np.save(os.path.join(data_array_directory, 'maggyr_time_domain_data_array.npy'), np.asarray(list_of_data_arrays_in_the_time_domain[4]))
-np.save(os.path.join(data_array_directory, 'maggyr_frequency_domain_data_array.npy'), np.asarray(list_of_data_arrays_in_the_frequency_domain[4]))
+    # 0 - "magacc"
+    np.save(os.path.join(data_array_directory,
+            f"magacc_{topic}_domain_data_array.npy"), np.asarray(font[0]))
+    # 123 - "acc_x_y_z_axes"
+    np.save(os.path.join(data_array_directory, f"acc_x_y_z_axes_{topic}_domain_data_array.npy"),
+            np.concatenate((np.asarray(font[1]), np.asarray(font[2]), np.asarray(font[3])), axis=2))
+    # 4 - "maggyr"
+    np.save(os.path.join(data_array_directory,
+            f"maggyr_{topic}_domain_data_array.npy"), np.asarray(font[4]))
 
-np.save(os.path.join(data_array_directory, 'gyr_x_y_z_axes_time_domain_data_array.npy'),
-        np.concatenate((np.asarray(list_of_data_arrays_in_the_time_domain[5]), np.asarray(list_of_data_arrays_in_the_time_domain[6]),
-                        np.asarray(list_of_data_arrays_in_the_time_domain[7])), axis=2))
+    # 567 - "gyr_x_y_z_axes"
+    np.save(os.path.join(data_array_directory, f"gyr_x_y_z_axes_{topic}_domain_data_array.npy"),
+            np.concatenate((np.asarray(font[5]), np.asarray(font[6]), np.asarray(font[7])), axis=2))
 
-np.save(os.path.join(data_array_directory, 'gyr_x_y_z_axes_frequency_domain_data_array.npy'),
-        np.concatenate((np.asarray(list_of_data_arrays_in_the_frequency_domain[5]), np.asarray(list_of_data_arrays_in_the_frequency_domain[6]),
-                        np.asarray(list_of_data_arrays_in_the_frequency_domain[7])), axis=2))
+    # 04 - magacc_and_maggyr
+    np.save(os.path.join(data_array_directory, f"magacc_and_maggyr_{topic}_domain_data_array.npy"),
+            np.concatenate((np.asarray(font[0]), np.asarray(font[4])), axis=2))
 
-np.save(os.path.join(data_array_directory, 'magacc_and_maggyr_time_domain_data_array.npy'),
-        np.concatenate((np.asarray(list_of_data_arrays_in_the_time_domain[0]), np.asarray(list_of_data_arrays_in_the_time_domain[4])), axis=2))
-
-np.save(os.path.join(data_array_directory, 'magacc_and_maggyr_frequency_domain_data_array.npy'),
-        np.concatenate((np.asarray(list_of_data_arrays_in_the_frequency_domain[0]), np.asarray(list_of_data_arrays_in_the_frequency_domain[4])), axis=2))
-
-np.save(os.path.join(data_array_directory, 'acc_and_gyr_three_axes_time_domain_data_array.npy'),
-        np.concatenate((np.asarray(list_of_data_arrays_in_the_time_domain[1]),np.asarray(list_of_data_arrays_in_the_time_domain[2]),
-                        np.asarray(list_of_data_arrays_in_the_time_domain[3]),np.asarray(list_of_data_arrays_in_the_time_domain[5]),
-                        np.asarray(list_of_data_arrays_in_the_time_domain[6]),np.asarray(list_of_data_arrays_in_the_time_domain[7])), axis=2))
-
-np.save(os.path.join(data_array_directory, 'acc_and_gyr_three_axes_frequency_domain_data_array.npy'),
-        np.concatenate((np.asarray(list_of_data_arrays_in_the_frequency_domain[1]), np.asarray(list_of_data_arrays_in_the_frequency_domain[2]),
-                        np.asarray(list_of_data_arrays_in_the_frequency_domain[3]),np.asarray(list_of_data_arrays_in_the_frequency_domain[5]),
-                        np.asarray(list_of_data_arrays_in_the_frequency_domain[6]),np.asarray(list_of_data_arrays_in_the_frequency_domain[7])), axis=2))
+    # 123567 - acc_and_gyr_three_axes
+    np.save(os.path.join(data_array_directory, f"acc_and_gyr_three_axes_{topic}_domain_data_array.npy"),
+            np.concatenate((
+                np.asarray(font[1]), np.asarray(font[2]),
+                np.asarray(font[3]), np.asarray(font[5]),
+                np.asarray(font[6]), np.asarray(font[7])),
+            axis=2))
+print("Finalizado. Dados de treinamento disponíveis.")
